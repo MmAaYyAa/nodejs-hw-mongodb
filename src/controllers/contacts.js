1eed269eddbc8f2a79b6b485342803da192c0a38
Maya Kazakova <anais.maya@gmail.com>
2024-10-10 18:37:14 +0100

    http-error

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,3 +1,5 @@
+import createHttpError from 'http-errors';
+
 import { getAllContacts, getContactById } from '../services/contacts.js';
 
 export const getContactsController = async (req, res) => {
@@ -15,11 +17,10 @@ export const getContactByIdController = async (req, res, next) => {
   const contact = await getContactById(contactId);
 
   if (!contact) {
-    next(new Error('Contact not found'));
-    return;
+    return next(createHttpError.NotFound(404, 'Contact not found'));
   }
 
-  res.status(200).json({
+  res.json({
     status: 200,
     message: `Successfully found contact with id ${contactId}!`,
     data: contact,
