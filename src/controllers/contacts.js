f222513b4a95c4007664dc2ea762a004a1a8a4c0
Maya Kazakova <anais.maya@gmail.com>
2024-10-29 12:31:54 +0100

    update

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -37,7 +37,7 @@ export const getContactsController = async (req, res) => {
 
 export const getContactByIdController = async (req, res, next) => {
   const { contactId } = req.params;
-  const contact = await getContactById(contactId);
+  const contact = await getContactById(contactId, req.user._Id);
 
   if (!contact) {
     throw createHttpError(404, 'Contact not found');
@@ -62,7 +62,7 @@ export const createContactController = async (req, res) => {
 
 export const deleteContactController = async (req, res, next) => {
   const { contactId } = req.params;
-  const contact = await deleteContact(contactId);
+  const contact = await deleteContact(contactId, req.user._id);
 
   if (!contact) {
     next(createHttpError(404, 'Contact not found'));
@@ -74,7 +74,7 @@ export const deleteContactController = async (req, res, next) => {
 
 export const patchContactController = async (req, res, next) => {
   const { contactId } = req.params;
-  const result = await updateContact(contactId, req.body);
+  const result = await updateContact(contactId, req.body, req.user._id);
 
   if (!result) {
     throw createHttpError(404, 'Contact not found');
