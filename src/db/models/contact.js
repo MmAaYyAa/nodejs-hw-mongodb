eee20de301dcfaec1d42b8e0b93a288fd4452d09
Maya Kazakova <anais.maya@gmail.com>
2024-11-09 21:27:42 +0100

    docs for routes GET and POST + swagger

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -26,7 +26,7 @@ const contactsSchema = new Schema(
     userId: {
       type: Schema.Types.ObjectId,
       ref: 'users',
-      //required: true,
+      required: true,
     },
     photo: {
       type: String,
--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import usersRouter from './routers/auth.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -15,6 +16,7 @@ export const setupServer = () => {
   const app = express();
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(express.json());
   app.use(cors());
